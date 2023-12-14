9c279bfb03a89059198aa378c7c217e28f38320b
Lars Stenberg <larste@waybler.com>
2023-12-14 18:04:54 +0100

    Added constructor for AtlasFieldAttribute

--- Fireflies.Atlas.Annotations/AtlasFieldAttribute.cs
+++ Fireflies.Atlas.Annotations/AtlasFieldAttribute.cs
@@ -1,5 +1,12 @@
 ﻿namespace Fireflies.Atlas.Annotations;
 
 public class AtlasFieldAttribute : AtlasAttribute {
+    public AtlasFieldAttribute() {
+    }
+
+    public AtlasFieldAttribute(string? name) {
+        Name = name;
+    }
+
     public string? Name { get; set; }
 }
